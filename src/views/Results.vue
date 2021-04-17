ad03c0199d9acba3aab8e6d244a3c4f67dbe3180
Zohamo <thiazsch@gmail.com>
2021-04-17 20:45:38 +0200

    feat(table) add results type

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="results">
+    <div class="filters">
+      <input
+        type="radio"
+        id="best"
+        value="best"
+        v-model="typeResults"
+        checked
+      />
+      <label for="best">Best</label>
+      <input type="radio" id="worst" value="worst" v-model="typeResults" />
+      <label for="worst">Worst</label>
+      <input type="radio" id="average" value="average" v-model="typeResults" />
+      <label for="average">Average</label>
+    </div>
     <Table :factions="factions" :results="results" />
   </div>
 </template>
@@ -21,15 +35,41 @@ export default defineComponent({
       factions: [] as Faction[],
       games: [] as Game[],
       results: [] as { [key: string]: any },
+      typeResults: "best",
+      bestResults: [] as { [key: string]: any },
+      worstResults: [] as { [key: string]: any },
+      averageResults: [] as { [key: string]: any },
     };
+  },
+  watch: {
+    typeResults(val) {
+      switch (val) {
+        case "worst":
+          if (!this.worstResults.length) {
+            this.worstResults = this.getWorstResults();
+          }
+          this.results = this.worstResults;
+          break;
+        case "average":
+          if (!this.averageResults.length) {
+            this.averageResults = this.getAverageResults();
+          }
+          this.results = this.averageResults;
+          break;
+        case "best":
+        default:
+          this.results = this.bestResults;
+      }
+    },
   },
   created() {
     this.factions = FactionService.getFactions();
     this.games = GameService.getGames();
-    this.results = this.getResults();
+    this.bestResults = this.getBestResults();
+    this.results = this.bestResults;
   },
   methods: {
-    getResults() {
+    getBestResults() {
       const results: { [key: string]: any } = [];
       this.games.forEach((game) => {
         if (!results[game.automa.faction]) {
@@ -43,6 +83,62 @@ export default defineComponent({
           results[game.automa.faction][game.human.faction] = game;
         }
       });
+      return results;
+    },
+    getWorstResults() {
+      const results: { [key: string]: any } = [];
+      this.games.forEach((game) => {
+        if (!results[game.automa.faction]) {
+          results[game.automa.faction] = [];
+        }
+        if (
+          !results[game.automa.faction][game.human.faction] ||
+          game.human.score <
+            results[game.automa.faction][game.human.faction].human.score
+        ) {
+          results[game.automa.faction][game.human.faction] = game;
+        }
+      });
+      return results;
+    },
+    getAverageResults() {
+      const results: { [key: string]: any } = [];
+      this.games.forEach((game) => {
+        if (!results[game.automa.faction]) {
+          results[game.automa.faction] = [];
+        }
+        if (!results[game.automa.faction][game.human.faction]) {
+          results[game.automa.faction][game.human.faction] = [];
+        }
+        results[game.automa.faction][game.human.faction].push({
+          human: game.human.score,
+          automa: game.automa.score,
+        });
+      });
+      for (const automaFaction in results) {
+        for (const humanFaction in results[automaFaction]) {
+          const scores = [...results[automaFaction][humanFaction]];
+          let humanScores = 0;
+          let automaScores = 0;
+          scores.forEach((score) => {
+            humanScores += score.human;
+            automaScores += score.automa;
+          });
+          const humanAverage = Math.round(humanScores / scores.length);
+          const automaAverage = Math.round(automaScores / scores.length);
+          results[automaFaction][humanFaction] = {};
+          results[automaFaction][humanFaction].human = {
+            faction: humanFaction,
+            score: humanAverage,
+            winner: humanAverage > automaAverage,
+          };
+          results[automaFaction][humanFaction].automa = {
+            faction: automaFaction,
+            score: automaAverage,
+            winner: automaAverage > humanAverage,
+          };
+        }
+      }
       return results;
     },
   },
